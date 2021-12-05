a146a945217eaf906d7916a1245cfe67e03086ee
Steeve <steeve.dombald.pro@outlook.com>
2021-12-05 15:53:37 +0100

    2nd commit

---
+++ paddle.js
@@ -0,0 +1,26 @@
+const SPEED = 0.007
+
+export default class paddle {
+
+    constructor(paddleElem) {
+        this.paddleElem = paddleElem
+    }
+
+
+    get position() {
+        return parseFloat(getComputedStyle(this.paddleElem).getPropertyValue("--position"))
+    }
+
+    set position(value) {
+        this.paddleElem.style.setProperty("--position", value)
+    }
+
+    reset() {
+        this.position = 50
+    }
+
+    update(delta, ballHeight) {
+        this.position += SPEED * delta * (ballHeight - this.position)
+    }
+
+}
---
+++ script.js
@@ -0,0 +1,50 @@
+import ball from './ball.js'
+import paddle from './paddle.js';
+
+
+const Ball = new ball(document.getElementById("ball"));
+
+const playerPaddle = new paddle(document.getElementById("player-paddle"))
+const computerPaddle = new paddle(document.getElementById("computer-paddle"))
+
+const playerScoreElem = document.getElementById("player-score")
+const computerScoreElem = document.getElementById("computer-score")
+
+
+let lastTime
+function update(time) {
+    if (lastTime != null) {
+        const delta = time - lastTime;
+        Ball.update(delta);
+        computerPaddle.update(delta, Ball.y)
+
+        // if (isLose()) handleLose()
+
+        
+    }
+    
+    lastTime = time;
+    window.requestAnimationFrame(update);
+}
+
+function isLose() {
+    const rect = Ball.rect();
+    return rect.right >= window.innerWidth || rect.left <= 0  
+}
+
+function handleLose() {
+    const rect = Ball.rect()
+    if (rect.right >= window.innerWidth) {
+        playerScoreElem.textContent = parseInt(playerScoreElem.textContent) + 1
+    } else {
+        computerScoreElem.textContent = parseInt(computerScoreElem.textContent) + 1
+    }
+    ball.reset()
+    computerPaddle.reset()
+}
+
+document.addEventListener("mousemove", e => {
+    playerPaddle.position = (e.y / window.innerHeight) * 100
+})
+
+window.requestAnimationFrame(update)
